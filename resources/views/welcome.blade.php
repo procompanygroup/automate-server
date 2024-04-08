22b680f48a06b2b9e0d822909aafa179d153ef29
najims2016 <70610331+ahmadms2020@users.noreply.github.com>
2024-04-09 01:26:07 +0200

    dash

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,16 +41,14 @@
                     </form>
              @endif
             
-                    <a href="{{ url('/cpanel') }}"  class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Cpanel</a>
+                    <a href="{{ url('/admin') }}"  class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                 
                     @auth
          
                     @else
                         <a href="{{ route('login') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
+                       
                     @endauth
                 </div>
             @endif
